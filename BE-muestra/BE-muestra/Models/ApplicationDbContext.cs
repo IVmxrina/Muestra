a1a598766dd04775de8e5faf38567aff3771128b
IVmxrina <marinacuemag@gmail.com>
2026-06-29 17:55:40 +0200

    Creacion del modelo Muestra

---
+++ BE-muestra/BE-muestra/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace BE_muestra.Models
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        { 
+        
+        }
+
+        public DbSet<Muestra> Muestras { get; set; } = null!;
+
+    }
+}
---
+++ BE-muestra/BE-muestra/Models/Muestra.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace BE_muestra.Models
+{
+    public class Muestra
+    {
+        [Key]
+        public int Id { get; set; }
+        public required string Nombre { get; set; }
+        public required string Descripcion { get; set; }
+
+        public required DateTime fechaCreacion { get; set; }
+    }
+}
