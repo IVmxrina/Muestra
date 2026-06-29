d58ed0bb2acfef0438264bfe2e0d828ab5b5017f
IVmxrina <marinacuemag@gmail.com>
2026-06-29 17:56:45 +0200

    Creacion del controlador Get para muestra

---
+++ BE-muestra/BE-muestra/Controllers/MuestraController.cs
@@ -0,0 +1,34 @@
+﻿using BE_muestra.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BE_muestra.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MuestraController : ControllerBase
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public MuestraController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var listMuestras = await _context.Muestras.ToListAsync();
+                return Ok(listMuestras);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
